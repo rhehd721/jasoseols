2e8bbe89a00591fa24e7a472d5e2af3be37a96e2
알파카 <rhehd721@naver.com>
2020-08-10 22:43:59 +0900

    자소설 닷컴 3~4강 추가

---
+++ newproject/app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Jasoseol
+
+# Create your models here.
+
+class JssForm(forms.ModelForm):
+    
+    class Meta:
+        model = Jasoseol
+        fields = ('title', 'content',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label = '제목'
+        self.fields['content'].label = '자기소개서 내용'
+        self.fields['title'].widget.attrs.update({
+            'class':'jss_title',
+            'placeholder' : '제목',
+        })
